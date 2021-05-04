2f699c1b9b83c729f3e55340a5b5a6a4a5a0963f
dupyo <ghdrnvy0922@naver.com>
2021-05-04 10:45:48 +0900

    2021-05-04 basic/OneDimenArray stage1

--- BaekjoonPRJ/src/basic/OneDimenArray.java
+++ BaekjoonPRJ/src/basic/OneDimenArray.java
@@ -10,7 +10,22 @@ public class OneDimenArray {
 		입력 : 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
 			모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
 		출력 : 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.*/
+		System.out.println(new OneDimenArray().stage1("20 10 35 30 7"));
 	}
 	
+	public String stage1(String arr) {
+		int min=1000000;
+		int max=-1000000;
+		for(String nStr : arr.split(" ")) {
+			int nInt=Integer.parseInt(nStr);
+			if(nInt < min)
+				min=nInt;
+			else {
+				if(nInt > max)
+					max=nInt;
+			}
+		}
+		return min+" "+max;
+	}
 
 }
